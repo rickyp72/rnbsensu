86e002c5e2c304c1b5fbcba65a7b172f64b575da
rickyp72 <ricky@email.com>
2016-11-17 19:24:07 +0000

    repo list file

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,18 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+cookbook_file '/etc/apt/sources.list.d/apt_repo.list' do
+  source 'apt_repo.list'
+  owner 'root'
+  group 'root'
+  mode 00644
+end
+
+execute 'update_apt' do
+  command 'apt-get update'
+  action :run
+end
+
 apt_package 'redis-server' do
   action :install
 end
